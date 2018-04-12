2b8511eb5987fcb3cc6e174c6cf73e23ef48ef2e
zubairadams <zubair.adams720@gmail.com>
2018-04-12 16:00:14 +0200

    text bill.js

--- js/text-bill.js
+++ js/text-bill.js
@@ -1,35 +1,38 @@
-// get a reference to the textbox where the bill type is to be entered
-var billTypeText = document.querySelector('.billTypeEntered')
-//get a reference to the add button
+var billTypeEntered = document.querySelector('.billTypeText')
 var addBtns = document.querySelector('.addToBillBtn')
-//create a variable that will keep track of the total bill
-var total0ne = document.querySelector('.totalOne')
-
-var smsTotals = document.querySelector('.smsTotal')
-
-var callTotals = document.querySelector('.callsTotal')
-//add an event listener for when the add button is pressed
+var totaltextBill = document.querySelector('.totalOne')
+var smsTotals = document.querySelector('.smsTotalOne')
+var callTotals = document.querySelector('.callTotalOne')
 var callsTotal = 0;
 var smsTotal = 0;
-//in the event listener check if the value in the bill type textbox is 'sms' or 'call'
-// * add the appropriate value to the running total
-// * add nothing for invalid values that is not 'call' or 'sms'.
-// * display the latest total on the screen
-function textBillTotal(){
-    // get the value entered in the billType textfield
-    var billTypeEntered = billTypeText.value.trim();
-    // update the correct total
-    if (billTypeEntered === "bill"){
-        callsTotal += 2.75
-    }
-    else if (billTypeEntered === "sms"){
-        smsTotal += 0.75;
-    }
+var totalBill = 0;
 
-    //update the totals that is displayed on the screen.
-    callsTotalElem.innerHTML = callsTotal.toFixed(2);
-    smsTotalElem.innerHTML = smsTotal.toFixed(2);
-    var totalCost = callsTotal + smsTotal;
-    totalCostElem.innerHTML = totalCost.toFixed(2);
-}
-addBtns.addEventListener('clicked',textBillTotal)
+function textBillTotal(){
+    var billItemOne = billTypeEntered.value.trim();
+    if (billItemOne === "call"){
+        callsTotal += 2.75;
+        callTotals.innerHTML= callsTotal.toFixed(2);
+        totalBill += 2.75;
+        totaltextBill.innerHTML = totalBill.toFixed(2);
+    }
+    else if (billItemOne === "sms"){
+      smsTotal += 0.75;
+      smsTotals.innerHTML= smsTotal.toFixed(2);
+      totalBill += 0.75;
+      totaltextBill.innerHTML = totalBill.toFixed(2);
+    }
+var totalCost = totalBill.toFixed(2);
+
+    console.log(1,totalCost.classList);
+
+    if (totalCost >= 50){
+          totaltextBill.classList.add("danger");
+      }
+      console.log(totaltextBill);
+      var cl = totaltextBill.classList;
+      console.log(cl);
+    if (totalCost >= 30){
+          totaltextBill.classList.add("warning");
+      }
+    }
+addBtns.addEventListener('click',textBillTotal);
