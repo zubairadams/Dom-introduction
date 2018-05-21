b79f5e329bde044729ab7e49abf5f0027c53fa12
zubairadams <zubair.adams720@gmail.com>
2018-05-21 14:44:43 +0200

    sigh

--- js/settings-bill.js
+++ js/settings-bill.js
@@ -1,61 +1,70 @@
+
 // get a reference to the sms or call radio buttons
 var call = document.querySelector("billItemTypeWithSettings");
 var sms = document.querySelector("billItemTypeWithSettings");
 
 // get refences to all the settings fields
-var callTotal = document.querySelector("callTotalSettings");
-var smsTotal = document.querySelector("smsTotalSettings");
-var totaltotal = document.querySelector("totalSettings");
-var criticalLevel = document.querySelector("u-full-width criticalLevelSetting");
-var danderLevel = document.querySelector("u-full-width warningLevelSetting ");
+var callTotal = document.querySelector(".callTotalSettings");
+var smsTotal = document.querySelector(".smsTotalSettings");
+var totaltotal = document.querySelector(".totalSettings");
+var criticalLevel = document.querySelector(".criticalLevelSetting");
+var warningLevel = document.querySelector(".warningLevelSetting ");
 
 //get a reference to the add button
-var radiobtn = document.querySelector("button-primary");
+var addBtn = document.querySelector("button-primary addbtn");
 
 //get a reference to the 'Update settings' button
-var settingsbtn = document.querySelector("button-primary updateSettings");
+var settingsbtn = document.querySelector(".updateSettings");
 
 // create a variables that will keep track of all the settings
 var callSettings = 0;
 var smsSettings = 0;
 var totalSettings = 0;
 var critical = 0;
-var danger = 0;
+var warning = 0;
 // create a variables that will keep track of all three totals.
 var calls = 0;
 var sms1 = 0;
 var totals = 0;
 
-function updateSetting(){
 
-
+function update(){
+callSettings = parsefloat(callTotal.value)
+smsSettings = parsefloat(smsTotal.value)
+totalSettings = parsefloat(totaltotal.value)
+critical = parsefloat(criticalLevel.value)
+warning = parsefloat(warningLevel.value)
 }
 
-function radioBillTotal(){
-  var check = "input[name='billItemType']:checked";
+function radioTotal(){
+  var checkradiobtn = document.querySelector("input[name='billItemTypeWithSettings']:checked");
 
-  if (check) {
-    var radio = check.value
-  }
-  var billItem = radio;
-  if (billItem === 'calls'){
-  call += callSettings;
-  }
-  else if (billItem === 'sms') {
-  sms += smsSettings;
-  }
- call.innerHTML = calls.toFixed(2);
- sms.innerHTML = sms1.toFixed(2);
+  if (checkradiobtn){
+    var radBtn = checkradiobtn.value
+    }
 
- var finalTotal = calls + sms1
-totaltotal.innerHTML = finalTotal.toFixed(2);
+    var Item = radBtn;
+    if (Item === "call"){
+      calls += callTotal
+    }
+    else if (Item === 'sms') {
+    sms1 += smsTotal
+  }
+    callTotal.innerHTML = calls.toFixed(2);
+    smsTotal.innerHTML = sms1.toFixed(2);
+    var total = totaltotal
+var total = sms1 + calls
+    totaltotal.innerHTML = total.toFixed(2);
 }
+
 
 
 //add an event listener for when the 'Update settings' button is pressed
-settingsbtn.addEventListener('click',updateSetting)
+settingsbtn.addEventListener('click',update)
+
 //add an event listener for when the add button is pressed
-addbtn.addEventListener('click',radioBillTotal)
+addBtn.addEventListener('click',radioTotal)
+
 //in the event listener get the value from the billItemTypeRadio radio buttons
 // * add the appropriate value to the call / sms total
 // * add the appropriate value to the overall total
