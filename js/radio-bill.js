57b0f058830c09bd38785581928f8d5723406a42
zubairadams <zubair.adams720@gmail.com>
2018-05-25 13:40:50 +0200

    work in progess

--- js/radio-bill.js
+++ js/radio-bill.js
@@ -1,53 +1,51 @@
+
+var Select = document.querySelector('.billItemTypeRadio');
+var radBtn = document.querySelector('.radioBillAddBtn');
+
+var callTwo = document.querySelector('.callTotalTwo');
+var smsTwo = document.querySelector('.smsTotalTwo');
+var totalRadio = document.querySelector('.totalTwo');
+
+
 var callsTotal3 = 0;
 var smsTotal3 = 0;
 var totalBill3 = 0;
-var Select = document.querySelector('.billItemTypeRadio')
-console.log(Select);
-var radBtn = document.querySelector('.radioBillAddBtn')
-
-var callTwo = document.querySelector('.callTotalTwo')
-var smsTwo = document.querySelector('.smsTotalTwo')
-var radioTotal = document.querySelector('.totalTwo')
 
 function radioBillTotal(){
-  var checkedRadioBtn = document.querySelector("input[name='billItemType']:checked");
 
+  var checkedRadioBtn = document.querySelector("input[name='billItemType']:checked");
   if (checkedRadioBtn){
-      var radBtn = checkedRadioBtn.value
+    var radBtn = checkedRadioBtn.value;
   }
   var billItemTwo = radBtn;
   if ( billItemTwo === 'call') {
     callsTotal3 += 2.75;
-
+  }else if(billItemTwo === 'sms'){
+    smsTotal3 += 0.75;
   }
-  else if(billItemTwo === 'sms'){
-  smsTotal3 += 0.75;
-
-  }
-    callTwo.innerHTML = callsTotal3.toFixed(2);
-    smsTwo.innerHTML = smsTotal3.toFixed(2);
-    //radioTotal.innerHTML = radioTotal.toFixed(2)
-    var totalBill = callsTotal3 + smsTotal3
-
-    radioTotal.innerHTML = totalBill.toFixed(2);
+  callTwo.innerHTML = callsTotal3.toFixed(2);
+  smsTwo.innerHTML = smsTotal3.toFixed(2);
+  totalBill3 = callsTotal3 + smsTotal3;
+  totalRadio.innerHTML = totalBill3.toFixed(2);
 
 
+    if (totalBill3 < 29){
+    totalRadio.classList.remove("warning")
+      totalRadio.classList.remove("danger")
+    }
+    else if (totalBill3 >= 50){
+      totalRadio.classList.remove("warning")
+      totalRadio.classList.add("danger")
 
-  if (totalBill < 29){
-  radioTotal.classList.remove("warning")
-    radioTotal.classList.remove("danger")
-  }
-  else if (totalBill >= 50){
-    radioTotal.classList.remove("warning")
-    radioTotal.classList.add("danger")
-
-  }
-else if (totalBill >= 30){
-  radioTotal.classList.remove("danger")
-  radioTotal.classList.add("warning")
-  }
-
-
-
+    }
+  else if (totalBill3 >= 30){
+    totalRadio.classList.remove("danger")
+    totalRadio.classList.add("warning")
+    }
 }
+
 radBtn.addEventListener('click',radioBillTotal);
+
+
+
+//
--- js/settings-bill.js
+++ js/settings-bill.js
@@ -1,15 +1,15 @@
 
 // get a reference to the sms or call radio buttons
-var call = document.querySelector("billItemTypeWithSettings");
-var sms = document.querySelector("billItemTypeWithSettings");
-
+var call = document.querySelector(".billItemTypeWithSettings");
 // get refences to all the settings fields
 var callTotal = document.querySelector(".callTotalSettings");
 var smsTotal = document.querySelector(".smsTotalSettings");
 var totaltotal = document.querySelector(".totalSettings");
+
+var callCost = document.querySelector(".callCostSetting");
+var smsCost = document.querySelector(".smsCostSetting");
 var criticalLevel = document.querySelector(".criticalLevelSetting");
 var warningLevel = document.querySelector(".warningLevelSetting ");
-
 //get a reference to the add button
 var addbtn = document.querySelector(".addbtn");
 
@@ -19,55 +19,60 @@ var settingsbtn = document.querySelector(".updateSettings");
 // create a variables that will keep track of all the settings
 var callSettings = 0;
 var smsSettings = 0;
-var totalSettings = 0;
 var critical = 0;
 var warning = 0;
 // create a variables that will keep track of all three totals.
-var calls = 0;
+var callBalance = 0;
 var sms1 = 0;
 var totals = 0;
 
-
-function update(){
-callSettings = parseFloat(callTotal.value)
-smsSettings = parseFloat(smsTotal.value)
-totalSettings = parseFloat(totaltotal.value)
-critical = parseFloat(criticalLevel.value)
-warning = parseFloat(warningLevel.value)
-}
+var no = warning + critical
 
 function radioTotal(){
   var checkradiobtn = document.querySelector("input[name='billItemTypeWithSettings']:checked");
-
-  if (checkradiobtn){
-    var radBtn = checkradiobtn.value
-    }
-
-    var Item = radBtn;
-    if (Item === "call"){
-      calls += callSettings
-    }
-    else if (Item === 'sms') {
-    sms1 += smsSettings
+  var radBtn = checkradiobtn.value.trim();
+  if (radBtn === 'call'){
+      callBalance += callSettings;
+  }else if (radBtn === 'sms'){
+      sms1 += smsSettings
   }
-    callTotal.innerHTML = calls.toFixed(2);
-    smsTotal.innerHTML = sms1.toFixed(2);
+  callTotal.innerHTML = callBalance.toFixed(2);
+  smsTotal.innerHTML = sms1.toFixed(2);
 
-var total = sms1 + calls
-    totaltotal.innerHTML = total.toFixed(2);
+  totals = sms1 + callBalance
+  totaltotal.innerHTML = totals.toFixed(2);
+
+// if (totals > no) {
+//   totaltotal.classList.remove("warning")
+//     totaltotal.classList.remove("danger")
+// }
+//   else
+if (totaltotal >= critical ){
+   totaltotal.classList.remove("warning")
+   totaltotal.classList.add("danger")
+
+
 }
+  else if (totals >= warning){
+  totaltotal.classList.add("warning")
+  //  totaltotal.classList.remove("danger")
+  }
 
+}
+function update(){
+  var newCallCost = callCost.value;
+  callSettings = parseFloat(newCallCost);
+  var newSmsCost = smsCost.value;
+  smsSettings = parseFloat(newSmsCost);
+  console.log(callSettings);
+  console.log(smsSettings);
 
+  critical = parseFloat(criticalLevel.value);
+  warning = parseFloat(warningLevel.value);
+}
 
 //add an event listener for when the 'Update settings' button is pressed
 settingsbtn.addEventListener('click',update)
 
 //add an event listener for when the add button is pressed
 addbtn.addEventListener('click',radioTotal)
-
-//in the event listener get the value from the billItemTypeRadio radio buttons
-// * add the appropriate value to the call / sms total
-// * add the appropriate value to the overall total
-// * add nothing for invalid values that is not 'call' or 'sms'.
-// * display the latest total on the screen.
-// * check the value thresholds and display the total value in the right color.
