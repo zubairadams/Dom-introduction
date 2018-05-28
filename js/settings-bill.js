9a96c107ccd987549ca68ff7ca5a4fb9bd22f96f
zubairadams <zubair.adams720@gmail.com>
2018-05-28 15:09:07 +0200

    changes

--- js/settings-bill.js
+++ js/settings-bill.js
@@ -1,74 +1,87 @@
 
 // get a reference to the sms or call radio buttons
-var call = document.querySelector(".billItemTypeWithSettings");
+const call = document.querySelector(".billItemTypeWithSettings");
 // get refences to all the settings fields
-var callTotal = document.querySelector(".callTotalSettings");
-var smsTotal = document.querySelector(".smsTotalSettings");
-var totaltotal = document.querySelector(".totalSettings");
+const callTotalElem = document.querySelector(".callTotalSettings");
+const smsTotalElem  = document.querySelector(".smsTotalSettings");
+const totaltotalElem = document.querySelector(".totalSettings");
 
-var callCost = document.querySelector(".callCostSetting");
-var smsCost = document.querySelector(".smsCostSetting");
-var criticalLevel = document.querySelector(".criticalLevelSetting");
-var warningLevel = document.querySelector(".warningLevelSetting ");
+const callCostElem  = document.querySelector(".callCostSetting");
+const smsCostElem = document.querySelector(".smsCostSetting");
+const criticalLevelElem = document.querySelector(".criticalLevelSetting");
+const warningLevelElem = document.querySelector(".warningLevelSetting ");
 //get a reference to the add button
-var addbtn = document.querySelector(".addbtn");
+const addbtnElem =  document.querySelector(".addbtn");
 
 //get a reference to the 'Update settings' button
-var settingsbtn = document.querySelector(".updateSettings");
+const settingsbtn = document.querySelector(".updateSettings");
+
+// Make sure all your DOM elements are const & add Elem to the end
 
 // create a variables that will keep track of all the settings
+
 var callSettings = 0;
 var smsSettings = 0;
 var critical = 0;
 var warning = 0;
+
 // create a variables that will keep track of all three totals.
 var callBalance = 0;
 var sms1 = 0;
 var totals = 0;
+
 function radioTotal(){
   var checkradiobtn = document.querySelector("input[name='billItemTypeWithSettings']:checked");
   var radBtn = checkradiobtn.value.trim();
   if (radBtn === 'call'){
       callBalance += callSettings;
+
   }
-  if (radBtn === 'sms'){
+  else if (totals >= critical){
+  return;
+  }
+ if (radBtn === 'sms'){
       sms1 += smsSettings
   }
-  callTotal.innerHTML = callBalance.toFixed(2);
-  smsTotal.innerHTML = sms1.toFixed(2);
+  else if (totals >= critical){
+  return;
+  }
+  callTotalElem.innerHTML = callBalance.toFixed(2);
+  smsTotalElem.innerHTML = sms1.toFixed(2);
 
   totals = sms1 + callBalance
-  totaltotal.innerHTML = totals.toFixed(2);
+  totaltotalElem.innerHTML = totals.toFixed(2);
 
- if (totaltotal >= critical ){
+
+ if (totaltotalElem >= critical ){
     totaltotal.classList.remove("warning")
     totaltotal.classList.add("danger")
-}
-else if (totals >= warning){
-totaltotal.classList.remove("danger")
-totaltotal.classList.add("warning")
+  }
+  else if (totals >= warning){
+  totaltotalElem.classList.add("warning")
 
+  }
+  else {
+    totaltotalElem.classList.remove("danger")
+    totaltotalElem.classList.remove("warning")
+  }
 }
 
-if (totals >= critical){
-totaltotal = critical;
 
-}
-}
 function update(){
-  var newCallCost = callCost.value;
+  var newCallCost = callCostElem.value;
   callSettings = parseFloat(newCallCost);
-  var newSmsCost = smsCost.value;
+  var newSmsCost = smsCostElem.value;
   smsSettings = parseFloat(newSmsCost);
   console.log(callSettings);
   console.log(smsSettings);
 
-  critical = parseFloat(criticalLevel.value);
-  warning = parseFloat(warningLevel.value);
+  critical = parseFloat(criticalLevelElem.value);
+  warning = parseFloat(warningLevelElem.value);
 }
 
 //add an event listener for when the 'Update settings' button is pressed
 settingsbtn.addEventListener('click',update)
 
 //add an event listener for when the add button is pressed
-addbtn.addEventListener('click',radioTotal)
+addbtnElem.addEventListener('click',radioTotal)
